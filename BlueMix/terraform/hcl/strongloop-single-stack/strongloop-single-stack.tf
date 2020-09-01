ee10211a0e278db447f6ed7b8b548eb2dec19f8e
bhadrim <bhadrim@ca.ibm.com>
2020-09-01 18:32:08 -0400

    Update strongloop-single-stack.tf

--- BlueMix/terraform/hcl/strongloop-single-stack/strongloop-single-stack.tf
+++ BlueMix/terraform/hcl/strongloop-single-stack/strongloop-single-stack.tf
@@ -151,7 +151,7 @@ make																			   >> $LOGFILE 2>&1 || { echo "---Failed to install node.
 sudo make install																   >> $LOGFILE 2>&1 || { echo "---Failed to install node.js---"| tee -a $LOGFILE; exit 1; }
 echo "---start installing compass---" | tee -a $LOGFILE 2>&1
 
-gem install -V compass                                                               >> $LOGFILE 2>&1
+#gem install -V compass                                                               >> $LOGFILE 2>&1
 
 echo "---finish installing compass and angularjs---" | tee -a $LOGFILE 2>&1
 
