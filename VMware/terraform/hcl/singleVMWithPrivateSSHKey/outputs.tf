ba66bc1cbb417f47c53f0c43712784176a8c03b2
bhadrim <bhadrim@ca.ibm.com>
2021-04-26 21:26:58 -0400

    Update outputs.tf

--- VMware/terraform/hcl/singleVMWithPrivateSSHKey/outputs.tf
+++ VMware/terraform/hcl/singleVMWithPrivateSSHKey/outputs.tf
@@ -1,8 +1,8 @@
-output "public_ssh_key" {
-  value = local.public_ssh_key
-}
+#output "public_ssh_key" {
+#  value = local.public_ssh_key
+#}
 
-output "private_ssh_key" {
-  value = local.private_ssh_key
-}
+#output "private_ssh_key" {
+#  value = local.private_ssh_key
+#}
 
